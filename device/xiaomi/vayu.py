b8ea8b4521aa54effd75f55777a21250d9f2293d
windeqk <windeqk@gmail.com>
2022-11-20 19:00:18 +0000

    device: xiaomi: vayu: Adapt for my kernel

--- device/xiaomi/vayu.py
+++ device/xiaomi/vayu.py
@@ -3,6 +3,6 @@ from device.xiaomi.sm8150 import XiaomiSM8150Device
 
 class XiaomiVayuDevice(XiaomiSM8150Device):
 	PRODUCT_DEVICE = "vayu"
-	TARGET_KERNEL_FRAGMENTS = ["vendor/xiaomi/sm8150-common.config", f"vendor/xiaomi/{PRODUCT_DEVICE}.config"]
+	TARGET_KERNEL_FRAGMENTS = ["vendor/vayu_defconfig"]
 
 register_device(XiaomiVayuDevice)
